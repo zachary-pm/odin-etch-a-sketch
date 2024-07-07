06fc71b26c096c48f1be280375ec3c02020d8c8d
zachary-pm <itsjustzach@gmail.com>
2024-07-07 16:35:48 -0400

    Create random color option and stlye buttons on bottom of screen

--- code.js
+++ code.js
@@ -1,35 +1,46 @@
 const maincontent = document.createElement("div");
 maincontent.id = "maincontent";
+const buttonBox = document.querySelector(".buttonBox");
 const body = document.querySelector(".container");
-body.appendChild(maincontent);
+body.insertBefore(maincontent, buttonBox);
 const reset = document.querySelector("#reset");
+let randomColorOn=false;
+
+const blackAndWhite = document.querySelector("#blackAndWhite");
+blackAndWhite.style.backgroundColor="darkgray";
+const unicornVomit = document.querySelector("#unicornVomit");
+
+blackAndWhite.addEventListener("click", ()=> {
+    randomColorOn = false;
+    unicornVomit.style.backgroundColor="lightgray";
+    blackAndWhite.style.backgroundColor="darkgray";
+
+});
+
+unicornVomit.addEventListener("click", ()=> {
+    randomColorOn = true;
+    unicornVomit.style.backgroundColor="darkgray";
+    blackAndWhite.style.backgroundColor="lightgray";
+
+});
+
 
 
 let fillColor = "black";
-setGrid(16);
-
+setGrid(50);
 
 reset.addEventListener("click", ()=> {
     sideLength = parseInt(prompt("Please enter a grid side length less than 100", 50));
-    if (sideLength > 100) {
-        alert("Too big!")    
+    if (sideLength > 100 || sideLength < 1 || sideLength == NaN) {
+        alert("Side length must be a number between 1 and 100!")    
         return;
     }
-
-    
 
     setGrid(sideLength);
 })
 
-
-    
-    
-
-
 function setGrid(sideLength) {
     maincontent.innerHTML="";
-
-
     for (let i=0; i<sideLength; i++) {
         const pixelRow = document.createElement("div");
         pixelRow.setAttribute("class", "pixelRow");
@@ -39,11 +50,17 @@ function setGrid(sideLength) {
             pixel.setAttribute("class", "pixel");
             pixelRow.appendChild(pixel);
             pixel.addEventListener("mouseenter", () => {
+                if (randomColorOn) {fillColor=randomizeColor();} else {fillColor="black";} 
                 pixel.style.backgroundColor = fillColor;
             })
-    
-    
         }
-    
     }
+}
+
+function randomizeColor(){
+    let randR = Math.floor(Math.random()*255);
+    let randG = Math.floor(Math.random()*255);
+    let randB = Math.floor(Math.random()*255);
+    randomColor=`rgb(${randR}, ${randG}, ${randB} )`;
+    return randomColor;
 }
